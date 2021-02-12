4c216df44f7e640aec1eaae1d1d14c2d03c0858b
MB <mb@blaster.ai>
2021-02-12 20:38:27 +0200

    Refactored config

--- src/upwork_feed_parser/config.clj
+++ src/upwork_feed_parser/config.clj
@@ -1,48 +1,42 @@
 (ns upwork-feed-parser.config
   (:require [clojure.string :as s]))
 
-(defn get-env-with-cast-and-default
+(defn env
   ([key]
-   (get-env-with-cast-and-default key identity nil))
-  ([key cast-fn]
-   (get-env-with-cast-and-default key cast-fn nil))
-  ([key default cast-fn]
-   (let [val (System/getenv key)]
-     (if val
-       (cast-fn val)
-       default))))
-
-(defn get-env-int
-  ([env]
-   (get-env-int env nil))
-  ([env default]
-   (get-env-with-cast-and-default env #(Integer/parseInt %) default)))
-
-(defn get-env-vector
-  ([env]
-   (get-env-vector env []))
-  ([env default]
-   (get-env-with-cast-and-default env default #(s/split % #" "))))
-
-(defn get-env
-  ([key]
-   (get-env key nil))
+   (env key nil))
   ([key default]
-   (or (System/getenv key) default)))
+   (let [val (or (System/getenv key) default)]
+     (if val
+       val
+       (throw (AssertionError. (str "Environment variable not set: " key)))))))
 
-(defn get-env-or-throw [key]
-  (let [val (get-env key)]
-    (if val
-      val
-      (throw (AssertionError. (str "Environment variable not set: " key))))))
+(defn env-and-cast
+  ([key cast-fn]
+   (env-and-cast key nil cast-fn))
+  ([key default cast-fn]
+   (let [val (env key default)]
+     (if (= val default)
+       default
+       (cast-fn val)))))
 
-(defonce security-token (get-env-or-throw "UPWORK_SECURITY_TOKEN"))
-(defonce user-id (get-env-or-throw "UPWORK_USER_ID"))
-(defonce org-uid (get-env-or-throw "UPWORK_USER_ID"))
-(defonce bot-token (get-env-or-throw "BOT_TOKEN"))
-(def rss-feed-ids (get-env-vector "UPWORK_FEED_IDS"))
-(def blacklisted-countries (get-env-vector "EXCLUDED_COUNTRIES"))
-(def min-hourly-budget (get-env-int "MIN_HOURLY_BUDGET" 45))
-(def min-fixed-budget (get-env-int "MIN_FIXED_BUDGET" 5000))
-(def redis-url (get-env "REDIS_URL" "redis://localhost:6379/"))
-(def sleep-between-runs (get-env-int "SLEEP_BETWEEN_RUNS" 1000))
+(defn env-cast-maker [cast-fn]
+  (fn
+    ([key]
+     (env-and-cast key nil cast-fn))
+    ([key default]
+     (env-and-cast key default cast-fn))))
+
+(def env-int (env-cast-maker #(Integer/parseInt %)))
+(def env-vec (env-cast-maker #(s/split % #" ")))
+
+(defonce security-token (env "UPWORK_SECURITY_TOKEN"))
+(defonce user-id (env "UPWORK_USER_ID"))
+(defonce org-uid (env "UPWORK_USER_ID"))
+(defonce bot-token (env "BOT_TOKEN"))
+(def rss-feed-ids (env-vec "UPWORK_FEED_IDS"))
+(def blacklisted-countries (env-vec "EXCLUDED_COUNTRIES"))
+(def min-hourly-budget (env-int "MIN_HOURLY_BUDGET" 45))
+(def min-fixed-budget (env-int "MIN_FIXED_BUDGET" 5000))
+(def redis-url (env "REDIS_URL" "redis://localhost:6379/"))
+(def sleep-between-runs (env-int "SLEEP_BETWEEN_RUNS" 1000))
+(def bot-token (env "BOT_TOKEN"))
