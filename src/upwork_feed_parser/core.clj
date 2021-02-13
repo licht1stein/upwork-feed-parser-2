a0b3a38bcc704270e6ba2d773ec027c77b4dfdef
MB <mb@blaster.ai>
2021-02-13 14:52:42 +0200

    Added start string

--- src/upwork_feed_parser/core.clj
+++ src/upwork_feed_parser/core.clj
@@ -19,7 +19,7 @@
 
 (defn -main
   [& args]
-  (println "Startin Upwork Feed Parser")
+  (println "Starting Upwork Feed Parser")
   (while true
     (let [old-checked-ids (get-checked-ids)
           result (runner old-checked-ids
